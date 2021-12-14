9d3a1dadbe3d1415d6e5f7fd446fc627b1794736
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-14 15:23:32 +0000

    Fetch 2 random dogs and pass dogInfo to VotingCard

---
+++ src/components/VotingCards.tsx
@@ -0,0 +1,42 @@
+import VotingCard from "./VotingCard";
+import { useEffect, useState } from "react";
+import { cleanAPIData } from "../utils/cleanAPIData";
+
+interface IDogRaw {
+  message: string[];
+  status: string;
+}
+
+interface IDogInfo {
+  breed: string;
+  subbreed: string | null;
+  URL: string;
+}
+
+export default function VotingCards(): JSX.Element {
+  //   const [firstLoad, setFirstLoad] = useState<boolean>(true);
+  const [dogInfo, setDogInfo] = useState<IDogInfo[]>();
+  const [dogVoted, setDogVoted] = useState<IDogInfo>();
+
+  useEffect(() => {
+    const getTwoDogs = () => {
+      fetch("https://dog.ceo/api/breeds/image/random/2")
+        .then((res) => res.json())
+        .then((data) => setDogInfo(data.message.map(cleanAPIData)));
+    };
+    getTwoDogs();
+  }, [dogVoted]);
+
+  console.log(dogInfo);
+
+  return (
+    <>
+      {dogInfo !== undefined &&
+        dogInfo.map((dog, idx) => (
+          <div key={idx}>
+            <VotingCard dog={dog} />
+          </div>
+        ))}
+    </>
+  );
+}
