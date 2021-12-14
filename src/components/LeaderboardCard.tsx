ebde61a71fc21af0ff53ec552637342e3016d24e
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-12-14 08:39:34 +0100

    Fix format issues in components and css files

--- src/components/LeaderboardCard.tsx
+++ src/components/LeaderboardCard.tsx
@@ -1,15 +1,15 @@
 // state that saves list of top 10 dog breeds
 
 function LeaderboardCard(): JSX.Element {
-  return  (
+  return (
     <>
-        <img                 
+      <img
         src="https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg"
         alt="leaderboard-card"
-        />
-        <h5>Dog Breed</h5>
-        <h6>Dog Sub-breed</h6>
-        <p>Num-Votes</p>
+      />
+      <h5>Dog Breed</h5>
+      <h6>Dog Sub-breed</h6>
+      <p>Num-Votes</p>
     </>
   );
 }
