6cb72775aca6293fc7c987fb2557b220ff1b6939
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-15 15:23:07 +0000

    Conditionally renders voting cards

--- src/components/VotingCard.tsx
+++ src/components/VotingCard.tsx
@@ -14,14 +14,21 @@ function VotingCard(props: {
     <button className="card-button" onClick={() => props.handleVote(props.dog)}>
       <img className="dog-image" src={props.dog.URL} alt="votingcard" />
       <div className="breed-name">
-        <h3>{props.dog.breed[0].toUpperCase() + props.dog.breed.substr(1)}</h3>
-        <p>
-          {props.dog.subbreed !== null ? (
-            props.dog.subbreed[0].toUpperCase() + props.dog.subbreed.substr(1)
-          ) : (
-            <br />
-          )}
-        </p>
+        {props.dog.breed !== undefined && (
+          <>
+            <p>
+              {props.dog.subbreed !== null ? (
+                props.dog.subbreed[0].toUpperCase() +
+                props.dog.subbreed.substr(1)
+              ) : (
+                <br />
+              )}
+            </p>
+            <h3>
+              {props.dog.breed[0].toUpperCase() + props.dog.breed.substr(1)}
+            </h3>
+          </>
+        )}
       </div>
     </button>
   );
